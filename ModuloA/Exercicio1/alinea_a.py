bc2e9dc9c796854ccf92053d4b92afd1563803ff
Amigo056 <161314089+Amigo056@users.noreply.github.com>
2026-03-11 16:03:04 +0000

    a done

--- ModuloA/Exercicio1/alinea_a.py
+++ ModuloA/Exercicio1/alinea_a.py
@@ -5,11 +5,13 @@ def multiplos_de_seis(a, a2):
     multiplos = []
     for num in range(a, a2 + 1):
         if num % 6 == 0:
-            multiplos.append(str(num))
-    return ', '.join(multiplos)
+            multiplos.append(num)
+    return multiplos
 
 def main():
-    test(multiplos_de_seis(1, 20), '6, 12, 18')
+    test(multiplos_de_seis(1, 20), [6, 12, 18])
+    test(multiplos_de_seis(10, 30), [12, 18, 24, 30])
+    test(multiplos_de_seis(0, 5), [0])
 
 if __name__ == "__main__":
     main()
