bab23e10e7730e4333a8f0aa8a42966a39aa4e33
Amigo056 <161314089+Amigo056@users.noreply.github.com>
2026-03-11 15:08:21 +0000

    d done

--- ModuloA/Exercicio1/alinea_a.py
+++ ModuloA/Exercicio1/alinea_a.py
@@ -1,13 +1,12 @@
 #(a) Determina e imprime na consola todos os números múltiplos de seis, contidos no intervalo definido por a e a2, inclusivamente
 from test import test
 
-
 def multiplos_de_seis(a, a2):
-    result = []
-    for i in range(a, a2 + 1):
-        if i % 6 == 0:
-            result.append(str(i))
-    return ', '.join(result)
+    multiplos = []
+    for num in range(a, a2 + 1):
+        if num % 6 == 0:
+            multiplos.append(str(num))
+    return ', '.join(multiplos)
 
 def main():
     test(multiplos_de_seis(1, 20), '6, 12, 18')
