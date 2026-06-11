8e0e3182267414eeb5f7981c21fcefab18df212a
Franciscoevora <112759682+Franciscoevor@users.noreply.github.com>
2026-06-11 18:52:12 +0100

    Add files via upload

--- ModuloB/Part2/pico/receptor.py
+++ ModuloB/Part2/pico/receptor.py
@@ -6,9 +6,9 @@ from burst_errors import burst_bit_error_bits
 from hamming import descodificar_texto_hamming             
 
 # Controlo do guião (parâmetro de entrada)
-USAR_HAMMING = True
+USAR_HAMMING = False
 USE_SINGLE_BIT_ERROR = True
-USE_BURST_BIT_ERROR = True
+USE_BURST_BIT_ERROR = False
 
 # Configuração da Porta Série 
 PORTA_ADAPTADOR = 'COM3'  
@@ -31,38 +31,37 @@ try:
     dados_bytes = ser.readline()
    
     if dados_bytes:
+        outFile = "dados_recebidos.txt"
         texto_recebido = dados_bytes.decode('utf-8').strip()
         print(f"\n[RECEBIDO] Texto puro do Pico: \"{texto_recebido}\"")
         
        
         
         if not USAR_HAMMING:
-            with open("dados_recebidos.txt", "w", encoding="utf-8") as f:
+            with open(outFile, "w", encoding="utf-8") as f:
                 f.write(texto_recebido + "\n")
             print("[FICHEIRO] Dados guardados com sucesso em 'dados_recebidos.txt'.")
           
         else:        
             dados_ba = bytearray(texto_recebido, "utf-8")
-            
         
             if USE_SINGLE_BIT_ERROR:
-           
+                outFile = "dados_com_erro_single.txt"
                 dados_ba = single_bit_error_bits(dados_ba, p=PROB)
                 print(f"Bits com erro aleatório: {dados_ba}")
 
             if USE_BURST_BIT_ERROR:
-              
+                outFile = "dados_com_erro_burst.txt"
                 dados_ba = burst_bit_error_bits(dados_ba, B=BURST)
                 print(f"Bits com erro aleatório: {dados_ba}")
            
             texto_descodificado, erros_detetados, erros_corrigidos = descodificar_texto_hamming(dados_ba)
-            print(f"Texto descodificado: {texto_descodificado}")
             print(f"Erros detetados: {erros_detetados}")
             print(f"Erros corrigidos: {erros_corrigidos}")
 
-        with open("dados_recebidos.txt", "w", encoding="utf-8") as f:
-            f.write(texto_descodificado + "\n")
-        print("[FICHEIRO] Dados guardados com sucesso em 'dados_recebidos.txt'.")
+            with open(outFile, "w", encoding="utf-8") as f:
+                f.write(texto_descodificado + "\n")
+            print(f"[FICHEIRO] Dados guardados com sucesso em {outFile}.")
     else:
         print("[AVISO] Timeout! Nenhum dado recebido do Pico 2 W.")
         
@@ -73,4 +72,3 @@ except KeyboardInterrupt:
 finally:
     ser.close()
     print("[SISTEMA] Porta série fechada com segurança.")
-
